f1ad14d6ea7c04cce574ed75bca134b739eeba37
israelnp <israelpereira1984@gmail.com>
2022-12-09 08:05:25 -0300

    first commit

---
+++ src/amplify-config.js
@@ -0,0 +1,7 @@
+const amplifyConfig = {
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_LUdcXitGW",
+    "aws_user_pools_web_client_id": "7485lhkbr0rn9mkctslij8857f"
+};
+
+export default amplifyConfig;
